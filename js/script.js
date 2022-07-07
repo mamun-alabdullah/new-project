d6fc818f7154b300bc27c94c5012242f62a80a1f
Abdullah Al mamun <81692712+mamun-alabdullah@users.noreply.github.com>
2022-07-07 11:06:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//Testimonial-slider start
+
+$('.servic-slider').slick({
+  dots: true,
+  prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+  nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>', 
+  infinite: true,
+  initialSlide:0,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+});
+
+
+//proatflo _venobox js start 
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+});
+
+//proatflo _venobox js end
+
+//proatflo mixitup js start
+
+var mixer = mixitup('.portflo-img');
+
+//Testimonial-slider start
+
+$('.Testimonial-slider').slick({
+  dots: true,
+  prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+  nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>', 
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: false,
+});
+
+//Testimonial-slider start
+
+
